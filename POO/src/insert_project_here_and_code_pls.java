af9055e74570705c0729d16b2db76efad979c504
João_Azevedo <jazevedo960@gmail.com>
2019-04-09 19:25:01 +0100

    src 1st file changed

---
+++ POO/src/insert_project_here_and_code_pls.java
@@ -0,0 +1,17 @@
+
+
+public class fazerOtrabalho {
+
+	private String codigo;
+	private int notaFinal;
+
+	public Trabalho() {
+		this.codigo = "nao tem";
+		this.notaFinal = 0;
+	}	
+
+
+	public boolean equals () {
+		return codigo == google.codigo();
+	}       	
+}
